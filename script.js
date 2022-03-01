dcba74f25758579e14a1f1a2a16f810f27e2945d
arnobtarafder <rmtarnob@gmail.com>
2022-03-01 22:53:59 +0600

    almost 90%

--- script.js
+++ script.js
@@ -61,12 +61,12 @@ const displaySearchResult = (products) => {
       const div = document.createElement("div");
       div.classList.add("col-12", "col-lg-4");
       div.innerHTML = `
-      <div class="card border-0 rounded-3 mx-auto shadow p-4" style="width:21rem">
-        <img src="${product.image}" class="card-img-top" alt="phone or watch or electric product image">
+      <div class="card border-0 rounded mx-auto shadow p-4" style="width:21rem">
+        <img src="${product.image}" class="card-img-top w-50 h-50 mx-auto mt-2 mb-3" alt="phone or watch or electric product image">
         <div class="card-body">
         <h5 class="card-title"> ${product.phone_name} </h5>
         <p class="card-text"> ${product.brand} </p>
-        <button onclick="getId('${product.slug}')" class="btn btn-primary">see more</button>  
+        <button onclick="getId('${product.slug}')" class="btn btn-primary">see details</button>  
         </div>
     </div>
       `;
@@ -78,6 +78,38 @@ const displaySearchResult = (products) => {
 }
 };
 
+  //   // load data 
+  //   const url = `https://openapi.programming-hero.com/api/phones?search=${searchField}`;
+  //     fetch(url)
+  //       .then(res => res.json())
+  //        .then(data => displaySearchResult(data.data))
+  
+
+  //      // remove old search result
+  //     productsDiv.textContent = '';
+        
+  //      // get every phone by forEach 
+  //     searchResult?.forEach (product => {
+  //      console.log(product.data);
+  //      const div = document.createElement("div");
+  //      div.classList.add("col-12", "col-lg-4");
+  //       div.innerHTML = `
+  //             <div class="card border-0 rounded mx-auto shadow p-4" style="width:21rem">
+  //               <img src="${product.image}" class="card-img-top w-50 h-50 mx-auto mt-2 mb-3" alt="phone or watch or electric product image">
+  //               <div class="card-body">
+  //               <h5 class="card-title"> ${product.phone_name} </h5>
+  //               <p class="card-text"> ${product.brand} </p>
+  //               <button onclick="getId('${product.slug}')" class="btn btn-primary">see details</button>  
+  //               </div>
+  //           </div>
+  //            `;
+              
+  //             productsDiv.appendChild(div);
+  //             // hide spinner 
+  //             document.getElementById("spinner").style.display = "none";
+  // })
+
+        
 
 
   // get product id dynamic url function
@@ -92,18 +124,19 @@ const getId = (id) => {
 // display product details function
 const displayDetails = (product) => {
   const detailsDiv = document.getElementById('product-details');
-  detailsDiv.textContent = '';
+  detailsDiv.textContent = "";
   const div = document.createElement('div');
   div.classList.add("row","shadow")
   div.innerHTML = `
-  <div class="p-3 col-12  col-lg-6 ">
+  <div class="p-3 col-12  col-lg-6">
       <h3 class="card-text">${product.name} </h3>
       <h5 class="text-danger">${product.releaseDate? product.releaseDate: 'release date not found'}</h5>
       <h5 class="card-text"> Brand: ${product.brand} </h5>
       <img src="${product.image}" class="card-img-top" alt=""/>
   </div>
+
   <div class=" p-3 col-12 col-lg-6 ">
-       <ul class="list-group">
+       <ul class="list-group mb-2">
           <h5 class="text-center text-info">Main Features</h5>
           <li class="list-group-item"><h6 class="card-text">ChipSet: ${product.mainFeatures.chipSet} </h6></li>
           <li class="list-group-item"><h6 class="card-text">Display: ${product.mainFeatures.displaySize} </h6></li>
@@ -112,21 +145,21 @@ const displayDetails = (product) => {
        
        <ul class="list-group">
           <h5 class="text-center text-info">Sensor Information</h5>
-          <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[0]? product.mainFeatures[0]: "not available"} </h6></li>
-          <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[1]? product.mainFeatures[1]: "not available"} </h6></li>
-          <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[2]? product.mainFeatures[2]: "not available"} </h6></li>
-          <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[3]? product.mainFeatures[3]: "not available"} </h6></li>
-          <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[4]? product.mainFeatures[4]: "not available"} </h6></li>
-          <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[5]? product.mainFeatures[5]: "not available"} </h6></li>
+          <li class="list-group-item"><h6 class="card-text"> ${product.mainFeatures.sensors[0]} </h6></li>
+          <li class="list-group-item"><h6 class="card-text"> ${product.mainFeatures.sensors[1]} </h6></li>
+          <li class="list-group-item"><h6 class="card-text"> ${product.mainFeatures.sensors[2]} </h6></li>
+          <li class="list-group-item"><h6 class="card-text"> ${product.mainFeatures.sensors[3]} </h6></li>
+          <li class="list-group-item"><h6 class="card-text"> ${product.mainFeatures.sensors[4]} </h6></li>
+          <li class="list-group-item"><h6 class="card-text"> ${product.mainFeatures.sensors[5]} </h6></li>
        </ul>
 
        <ul class="list-group">
           <h5 class="text-center text-info"> Others Information </h5>
-          <li class="list-group-item"><h6 class="card-text">Bluetooth: ${product.others?.Bluetooth} </h6></li>
-          <li class="list-group-item"><h6 class="card-text">GPS: ${product.others?.GPS} </h6></li>
-          <li class="list-group-item"><h6 class="card-text">Radio: ${product.others?.Radio} </h6></li>
-          <li class="list-group-item"><h6 class="card-text">USB: ${product.others?.USB} </h6></li>
-          <li class="list-group-item"><h6 class="card-text">WLAN: ${product.others?.WLAN} </h6></li>
+          <li class="list-group-item"><h6 class="card-text">Bluetooth: ${product?.others?.Bluetooth? product?.others?.Bluetooth : "not available"} </h6></li>
+          <li class="list-group-item"><h6 class="card-text">GPS: ${product?.others?.Bluetooth? product?.others?.GPS : "not available"} </h6></li>
+          <li class="list-group-item"><h6 class="card-text">Radio: ${product?.others?.Bluetooth? product?.others?.Radio : "not available"} </h6></li>
+          <li class="list-group-item"><h6 class="card-text">USB: ${product?.others?.Bluetooth? product?.others?.USB : "not available"} </h6></li>
+          <li class="list-group-item"><h6 class="card-text">WLAN: ${product?.others?.Bluetooth? product?.others?.WLAN : "not available"} </h6></li>
        </ul>
   </div>
   `;
